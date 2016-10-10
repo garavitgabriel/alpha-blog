fa0faed181afed07a7535a83d6c9f3d54a920549
Gabriel Garavit <garavitgabriel@gmail.com>
2016-10-09 20:19:23 -0500

    Add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ validates :username, presence: true, uniqueness: { case_sensitive: false },
 	validates :email, presence: true, length: {maximum:105},
  	uniqueness: {case_sensitive: false },
  	format: {with: VALID_EMAIL_REGEX}
+ 	has_secure_password
 
 end
