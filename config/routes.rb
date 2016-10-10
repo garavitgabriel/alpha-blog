60ba43575cb64932fad3fa1afba444b85d31b75e
Gabriel Garavit <garavitgabriel@gmail.com>
2016-10-10 14:18:21 -0500

    Add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ resources :articles
 get'signup', to:'users#new'
 post 'users', to: 'users#create'
 
+resources :users 
+
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
